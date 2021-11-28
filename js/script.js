e8027e5a852bb488637acf0d4c01d7ea39c676e9
sarahschuller <sarahmichele91@gmail.com>
2021-11-27 20:15:20 -0500

    converted part 1 of js script to jQuery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function newItem(){
+/* function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items:
@@ -34,4 +34,15 @@ function newItem(){
  // 4. Reordering the items:
    $('#list').sortable();
 
+}
+*/
+
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === '') {
+  alert ('You must write something!');
+} else {
+  $('#list').append(li);
 }
