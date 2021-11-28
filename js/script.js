c13e99564c066892d7d8ef06353ace5fe8b2580a
sarahschuller <sarahmichele91@gmail.com>
2021-11-27 20:56:22 -0500

    converted remainder of js code to jQuery and corrected double click crossout function

--- js/script.js
+++ js/script.js
@@ -37,6 +37,9 @@
 }
 */
 
+//add new item to list
+function newItem(){
+
 let li = $('<li></li>');
 let inputValue = $('#input').val();
 li.append(inputValue);
@@ -45,12 +48,29 @@ if (inputValue === '') {
   alert ('You must write something!');
 } else {
   $('#list').append(li);
+  $('#input').val('');
 }
 
+//strike out item on list
 function crossOut(){
   li.toggleClass ('strike')
 }
 
-li.on('dbclick', function (crossOut){
+li.on('dbclick', function crossOut(){
   li.toggleClass('strike');
 });
+
+//add delete button to list items
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append('X');
+li.append(crossOutButton);
+
+//delete item from list
+crossOutButton.on('click', deleteListItem);
+function deleteListItem(){
+  li.addClass('delete')
+}
+
+//make list sortable
+$('#list').sortable();
+}
